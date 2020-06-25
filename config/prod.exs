19b7d5c33e689cffd8b9bc5507cb8e8580c452ca
Roman Berdichevskii <roman.berdichevskii@bettyblocks.com>
2020-06-25 21:15:31 +0200

    chore: remove browser cache manifest and update default port

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gpanel_one_page, GpanelOnePageWeb.Endpoint,
-  url: [host: "kintu.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "kintu.com", port: 4004]
 
 # Do not print debug messages in production
 config :logger, level: :info
